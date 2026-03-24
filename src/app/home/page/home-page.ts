ac1546e47eb1e35584447f6652fc1712482aaa97
NadiaMonzon <nadiamonque99@gmail.com>
2026-03-24 16:35:26 +0000

    feat(home-page): implement player creation form with validation and routing

--- src/app/home/page/home-page.ts
+++ src/app/home/page/home-page.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.html',
+  styleUrl: './home-page.scss',
+  imports: [RouterLink, FormsModule],
 })
-export class HomePage {}
+export class HomePage {
+  name = signal('');
+  isValid = computed(() => this.name().trim().length > 0);
+
+  onNameInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.name.set(input.value);
+  }
+}
