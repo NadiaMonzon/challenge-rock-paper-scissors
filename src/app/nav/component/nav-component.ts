e82afbead2c2ee898d596c8841731f97528799c0
NadiaMonzon <nadiamonque99@gmail.com>
2026-03-24 22:11:11 +0000

    feat(nav): display current player's name in navigation bar

--- src/app/nav/component/nav-component.ts
+++ src/app/nav/component/nav-component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { PlayerSessionService } from '../../player/services/PlayerSessionService';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav-component.html',
   imports: [RouterLink, RouterOutlet],
 })
-export class NavComponent {}
+export class NavComponent {
+  protected playerSession = inject(PlayerSessionService);
+}
