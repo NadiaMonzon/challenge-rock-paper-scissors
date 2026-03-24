1475a2d11194935b050ff443d9ac605f71fc1221
NadiaMonzon <nadiamonque99@gmail.com>
2026-03-24 14:38:44 +0000

    test(app): update tests to focus on rendering router outlet

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -8,16 +8,9 @@ describe('App', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should render the router outlet', () => {
     const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, challenge-rock-paper-scissors');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
