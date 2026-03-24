31a61cd7757cd5ca393936933d7cae5a42893000
NadiaMonzon <nadiamonque99@gmail.com>
2026-03-24 16:35:15 +0000

    feat(home-page): update routing to use dynamic component loading for HomePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './home/page/home-page';
 
-export const routes: Routes = [{ path: '', component: HomePage }];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./home/page/home-page').then((m) => m.HomePage) },
+];
