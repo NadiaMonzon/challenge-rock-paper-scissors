77e9c29717908541b35d490a38a21ae013b0f8d5
NadiaMonzon <nadiamonque99@gmail.com>
2026-03-24 19:59:20 +0000

    feat(storage): implement StorageRepository and LocalStorageRepository classes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import type { ApplicationConfig } from '@angular/core';
+import { isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { routes } from './app.routes';
+import { LocalStorageRepository } from './shared/storage/repositories/LocalStorageRepository';
+import { StorageRepository } from './shared/storage/repositories/StorageRepository';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    { provide: StorageRepository, useClass: LocalStorageRepository },
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
