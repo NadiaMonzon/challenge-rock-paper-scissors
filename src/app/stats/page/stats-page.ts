2ccb710c643d811383de4562151e192a675df471
NadiaMonzon <nadiamonque99@gmail.com>
2026-03-26 21:04:23 +0000

    feat(stats): implement player statistics display

--- src/app/stats/page/stats-page.ts
+++ src/app/stats/page/stats-page.ts
@@ -1,7 +1,35 @@
-import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import type { PlayerModel } from '../../player/models/PlayerModel';
+import { LocalStoragePlayerRepository } from '../../player/repositories/LocalStoragePlayerRepository';
+import { GetPlayerListQuery } from '../../player/services/GetPlayerListQuery';
 
 @Component({
   selector: 'app-stats-page',
   templateUrl: './stats-page.html',
+  providers: [
+    {
+      provide: GetPlayerListQuery,
+      useClass: GetPlayerListQuery,
+      deps: [LocalStoragePlayerRepository],
+    },
+    {
+      provide: LocalStoragePlayerRepository,
+      useClass: LocalStoragePlayerRepository,
+    },
+  ],
 })
-export class StatsPage {}
+export class StatsPage implements OnInit {
+  private getPlayerListQuery = inject(GetPlayerListQuery);
+
+  protected playerList = signal<PlayerModel[]>([]);
+
+  ngOnInit(): void {
+    this.getPlayerList();
+  }
+
+  protected getPlayerList(): void {
+    const playerList = this.getPlayerListQuery.execute({ sort: { by: 'score', order: 'desc' } });
+    this.playerList.set(playerList);
+  }
+}
