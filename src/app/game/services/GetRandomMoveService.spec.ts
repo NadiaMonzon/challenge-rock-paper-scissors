ff18298d459e8b6570070c3d437db79cf089a994
NadiaMonzon <nadiamonque99@gmail.com>
2026-03-26 15:53:01 +0000

    feat(game): implement Lizard-Spock game mode and update move selection logic

--- src/app/game/services/GetRandomMoveService.spec.ts
+++ src/app/game/services/GetRandomMoveService.spec.ts
@@ -11,13 +11,13 @@ describe('GetRandomMoveService', () => {
   });
 
   it('should return a valid game move', () => {
-    const move = service.execute();
+    const move = service.execute(false);
     expect(VALID_MOVES).toContain(move);
   });
 
   it('should return one of the 5 possible moves over multiple calls', () => {
     for (let i = 0; i < 50; i++) {
-      expect(VALID_MOVES).toContain(service.execute());
+      expect(VALID_MOVES).toContain(service.execute(false));
     }
   });
 });
